8df7f816ac0aad7a08d94438d4ac27a746dce62c
user <user@DESKTOP-6JBEF5K>
2022-04-11 11:31:35 +0800

    1

--- WindowsFormsApp0411/Form1.Designer.cs
+++ WindowsFormsApp0411/Form1.Designer.cs
@@ -32,9 +32,10 @@ namespace WindowsFormsApp0411
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox_ip = new System.Windows.Forms.TextBox();
+            this.textBox_target = new System.Windows.Forms.TextBox();
             this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -42,62 +43,78 @@ namespace WindowsFormsApp0411
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(69, 65);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(33, 12);
+            this.label1.Size = new System.Drawing.Size(39, 12);
             this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
+            this.label1.Text = "目標IP";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(324, 65);
+            this.label2.Location = new System.Drawing.Point(240, 65);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(33, 12);
+            this.label2.Size = new System.Drawing.Size(48, 12);
             this.label2.TabIndex = 1;
-            this.label2.Text = "label2";
+            this.label2.Text = "目標Port";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(566, 65);
+            this.label3.Location = new System.Drawing.Point(436, 65);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(33, 12);
+            this.label3.Size = new System.Drawing.Size(48, 12);
             this.label3.TabIndex = 2;
-            this.label3.Text = "label3";
+            this.label3.Text = "接聽port";
             // 
-            // textBox1
+            // textBox_ip
             // 
-            this.textBox1.Location = new System.Drawing.Point(108, 62);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 22);
-            this.textBox1.TabIndex = 3;
+            this.textBox_ip.Location = new System.Drawing.Point(108, 62);
+            this.textBox_ip.Name = "textBox_ip";
+            this.textBox_ip.Size = new System.Drawing.Size(100, 22);
+            this.textBox_ip.TabIndex = 3;
             // 
-            // textBox2
+            // textBox_target
             // 
-            this.textBox2.Location = new System.Drawing.Point(363, 62);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 22);
-            this.textBox2.TabIndex = 4;
+            this.textBox_target.Location = new System.Drawing.Point(294, 62);
+            this.textBox_target.Name = "textBox_target";
+            this.textBox_target.Size = new System.Drawing.Size(100, 22);
+            this.textBox_target.TabIndex = 4;
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(605, 62);
+            this.textBox3.Location = new System.Drawing.Point(490, 62);
             this.textBox3.Name = "textBox3";
             this.textBox3.Size = new System.Drawing.Size(100, 22);
             this.textBox3.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(647, 62);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "開始";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.textBox_target);
+            this.Controls.Add(this.textBox_ip);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -108,9 +125,10 @@ namespace WindowsFormsApp0411
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox_ip;
+        private System.Windows.Forms.TextBox textBox_target;
         private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- WindowsFormsApp0411/Form1.cs
+++ WindowsFormsApp0411/Form1.cs
@@ -8,13 +8,117 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using Microsoft.VisualBasic.PowerPacks;
+
 namespace WindowsFormsApp0411
 {
     public partial class Form1 : Form
     {
+        UdpClient U;
+        Thread th;
+        ShapeContainer C;
+        ShapeContainer D;
+        Point stP;
+        string p;
         public Form1()
         {
             InitializeComponent();
         }
+        private void Listen()
+        {
+            int Port = int.Parse(textBox3.Text);
+            U = new UdpClient(Port);
+            IPEndPoint EP = new IPEndPoint(IPAddress.Parse("127.0.0.1"), Port);
+            while (true)
+            {
+                byte[] B = U.Receive(ref EP);
+                string A = Encoding.Default.GetString(B);
+                string[] Q = A.Split('/');
+                Point[] R = new Point[Q.Length];
+                for(int i = 0; i < Q.Length; i++)
+                {
+                    string[] K = Q[i].Split(',');
+                    R[i].X = int.Parse(K[0]);
+                    R[i].Y = int.Parse(K[1]);
+                }
+                
+                for(int i = 0; i < Q.Length - 1; i++)
+                {
+                    LineShape L = new LineShape();
+                    L.StartPoint = R[i];
+                    L.EndPoint = R[i + 1];
+                    L.Parent = D;
+                }
+                Thread.Sleep(2000);
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            C = new ShapeContainer();
+            this.Controls.Add(C);
+            D = new ShapeContainer();
+            this.Controls.Add(D);
+
+        }
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            try
+            {
+                th.Abort();
+                U.Close();
+            }
+            catch
+            {
+
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Control.CheckForIllegalCrossThreadCalls = false;
+            th = new Thread(Listen);
+            th.Start();
+            button1.Enabled = false;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            stP = e.Location;
+            p = stP.X.ToString() + "," + stP.Y.ToString();
+
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == System.Windows.Forms.MouseButtons.Left)
+            {
+                LineShape L = new LineShape();
+                L.StartPoint = stP;
+                L.EndPoint = e.Location;
+                L.Parent = C;
+                stP = e.Location;
+                p += "/" + stP.X.ToString() + "," + stP.Y.ToString();
+            }
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            int Port = int.Parse(textBox_target.Text);
+            UdpClient S = new UdpClient(textBox_ip.Text, Port);
+            byte[] B = Encoding.Default.GetBytes(p);
+            S.Send(B, B.Length);
+            S.Close();
+        }
+
+        
+      
     }
 }
